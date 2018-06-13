bc7b6437634576cbcfe29ccf237b8592bc2b8e0a
MC <marisol@rindecuentas.org>
2018-06-13 14:11:19 -0500

    Añadí sección de Completar el perfil #86 Quité tabs de Personas y Organizaciones #71 y #72

--- src/imports/ui/pages/contracts/contracts.js
+++ src/imports/ui/pages/contracts/contracts.js
@@ -112,7 +112,7 @@ Template.contractProfileImage.helpers({
 
 Template.contractView.onRendered(function() {
   this.$('[data-toggle="tooltip"]').tooltip({placement: 'right'});
-  $('.right-menu-contracts').affix({offset: {top: 280} }); 
+  $('.right-menu-contracts').affix({offset: {top: 280, bottom:850} }); 
   import("../../../../node_modules/jquery.easing/jquery.easing.js").then(() => {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
