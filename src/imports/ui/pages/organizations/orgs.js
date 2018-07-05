d0525b5f47427888c16466c0ab1efe900d8da594
MC <marisol@rindecuentas.org>
2018-07-05 12:04:21 -0500

    Actualización del treemap

--- src/imports/ui/pages/organizations/orgs.js
+++ src/imports/ui/pages/organizations/orgs.js
@@ -231,10 +231,10 @@ Template.orgView.onRendered(function() {
           return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();;
         }
       })
-      .colorRandom("category20")
       //.color({
         //"scale": "category20"
         //"range": [ "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5" ],
+        //"range": ["#B22200", "#FFEE8D", "#759143" ],
         //"value": "growth"
       //})  
       .sum("value")
