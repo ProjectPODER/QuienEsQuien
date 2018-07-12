c10b13b8bdc1943391495ba7659aa30ac0e3cd30
Martín S <martin@rindecuentas.org>
2018-07-12 17:04:10 -0500

    WIP: Cableando persons, pero no aparecen las memberships #75

--- src/imports/ui/pages/personas/persons.js
+++ src/imports/ui/pages/personas/persons.js
@@ -5,6 +5,7 @@ import { Session } from 'meteor/session';
 import i18n from 'meteor/universe:i18n';
 import { Notifications } from 'meteor/gfk:notifications';
 import { Persons } from '../../../api/persons/persons.js';
+import Memberships from '../../../api/memberships/memberships';
 import '../../components/memberships/memberships.js';
 import '../../components/visualizations/viz.js';
 import '../../components/similar/similar.js';
@@ -42,11 +43,12 @@ Template.showPersonWrapper.onCreated(function () {
   const self = this;
 
   self.person = new ReactiveVar(false);
+  self.memberships = new ReactiveVar(false);
   self.ready = new ReactiveVar(false);
 
   self.autorun(() => {
     const id = FlowRouter.getParam('_id');
-    DocHead.setTitle(`QQW - ${id}`);
+    DocHead.setTitle(`QuienEsQuien.wiki - ${id}`);
     const handle = self.subscribe('person', id, {
       onReady() {
         const person = Persons.findOne({
@@ -54,6 +56,11 @@ Template.showPersonWrapper.onCreated(function () {
         });
         Session.set('currentDocumentId', person._id);
         self.person.set(person);
+        const membership = Memberships.find({
+          "person_id": id,
+        })
+        self.memberships.set(membership.fetch());
+        console.log("membership",id,membership)
       },
     });
     self.ready.set(handle.ready());
@@ -68,6 +75,12 @@ Template.showPersonWrapper.helpers({
     var person = Template.instance().person.get();
     person.collection = 'persons';
     return person;
+  },
+  memberships: function() {
+    return Template.instance().memberships.get();
+  },
+  has_memberships: function() {
+    return Template.instance().memberships.get().length > 0;
   }
 });
 
