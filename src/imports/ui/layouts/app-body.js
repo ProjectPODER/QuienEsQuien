f5c95d8a2429fe2c46cd3d35c9193bb1c0e0b6ae
Martín S <martin@rindecuentas.org>
2018-07-22 12:17:35 -0500

    Fix template reference

--- src/imports/ui/layouts/app-body.js
+++ src/imports/ui/layouts/app-body.js
@@ -1,8 +1,6 @@
 import '../components/nav/nav.js';
 import '../components/search/search.js';
-import '../components/newVersionNotification.html';
-import '../components/newVersionNotification.js';
-import '../components/header.html';
+import '../components/header/header.html';
 import '../components/footer.html';
 import '../components/spin/spinner.html';
 import './app-body.html';
