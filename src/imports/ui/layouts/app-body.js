53134d184b19c013191e86d44015dd597b503e04
MC <marisol@rindecuentas.org>
2018-07-30 17:19:23 -0500

    Cambios en el footer #134

--- src/imports/ui/layouts/app-body.js
+++ src/imports/ui/layouts/app-body.js
@@ -16,7 +16,32 @@ Meteor.startup(function(){
       template.onRendered(function(){
         // $('[data-toggle="tooltip"]').tooltip({placement: 'right'});
         $('nav').addClass('fixed-nav');
-        $(".owl-carousel").owlCarousel();
+        $('.owl-carousel').owlCarousel({
+            // center: true,
+            items:3,
+            loop:true,
+            margin:0,
+            dots:false,
+            nav:true,
+            navText:['<','>'],
+            autoplay:true,
+            autoplayTimeout:3000,
+            autoplayHoverPause:true,
+            responsive:{
+                1042:{
+                    items:3
+                },
+                992:{
+                    items:2
+                },
+                300:{
+                    items:1
+                },
+                0:{
+                    items:1
+                }
+            }
+        });
       });
     }
   }
