26ee27213098fb38522ffd8525c5412d40f8ba5d
MC <marisol@rindecuentas.org>
2018-08-09 17:54:06 -0500

    Arreglo de hash links #148

--- src/imports/ui/layouts/app-body.js
+++ src/imports/ui/layouts/app-body.js
@@ -44,29 +44,6 @@ Meteor.startup(function(){
                 }
             }
         });
-        // Code to link to anchor hashes within a page
-            var scrollToHash = function  (hash, time) {
-              hash = window.location.hash;
-              time = time || 200;
-
-              // If the link goes to the same (home) page
-              if (hash == '/') {
-                $('body').animate({
-                  scrollTop: 0
-                }, time);  
-              }
-              else if ($(hash).length) {
-                $('body').animate({
-                  scrollTop: $(hash).offset().top
-                }, time);
-                console.log(scrollToHash);
-              }
-              else {
-                $('body').animate({
-                  scrollTop: 0
-                }, time);
-              }
-            };
       });
     }
   }
--- src/imports/ui/pages/about/about.js
+++ src/imports/ui/pages/about/about.js
@@ -3,3 +3,16 @@ import i18n from 'meteor/universe:i18n';
 import './about.html';
 // import jquery from 'jquery.easing';
 
+Template.acerca_de.onRendered(function() {
+  var hash = window.location.hash;
+      if ($(hash).length) {  // if hash exists on page
+        $('html,body').animate({
+          scrollTop: $(hash).offset().top - 90
+        }, 200);
+      };
+  
+  /*$('a.anchor-scroll[href*=\\#]').on('click', function(event){     
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top - 90 }, 200);
+  });*/
+});
